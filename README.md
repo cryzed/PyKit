3b6012fc25b02e8733714396e29017ddd261bdcc
cryzed <cryzed@googlemail.com>
2014-05-02 10:34:49 +0200

    Added a find_pixel function which currently works very slowly

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 PyKit
 =====
-
 PyKit is an automation library for Windows, supporting 32 and 64-bit applications. It currently supports controlling the cursor and detouring/hooking functions in foreign processes to local Python code.
 
 
--- pykit/screen.py
+++ pykit/screen.py
@@ -6,7 +6,8 @@ user32 = ctypes.windll.user32
 gdi = ctypes.windll.gdi32
 
 
-def _make_COLORREF(r, g, b):
+def _make_COLORREF(color):
+    r, g, b = color
     return r | g << 8 | b << 16
 
 
@@ -18,21 +19,38 @@ class Screen(object):
     def __init__(self):
         self._dc = user32.GetDC(0)
         self._cursor = Cursor()
+        self.width = user32.GetSystemMetrics(0)
+        self.height = user32.GetSystemMetrics(1)
 
-    def get_pixel_color(self, x=None, y=None):
-        if x or y is None:
-            current_x, current_y = self._cursor.get_position()
-            x = x or current_x
-            y = y or current_y
-
-        colorref = gdi.GetPixel(self._dc, x, y)
+    def get_pixel_color(self, position=None):
+        position = position or self._cursor.get_position()
+        colorref = gdi.GetPixel(self._dc, *position)
         return _unpack_COLORREF(colorref)
 
-    def set_pixel_color(self, r, g, b, x=None, y=None):
-        if x or y is None:
-            current_x, current_y = self._cursor.get_position()
-            x = x or current_x
-            y = y or current_y
+    # TODO: This is terribly inefficient -- needs to be improved by taking a
+    # screenshot of the region and searching for the correct pixel (PIL?)
+    def find_pixel(self, color, region=None, limit=1, step_x=1, step_y=1):
+        locations = []
+        region = region or ((0, 0), (self.width - 1, self.height - 1))
 
-        colorref = _make_COLORREF(r, g, b)
-        gdi.SetPixel(self._dc, x, y, colorref)
+        x1, y1 = region[0]
+        x2, y2 = region[1]
+        x, y = x1, y1
+
+        while (not limit or len(locations) < limit) and y <= y2:
+            print x, y
+            color_ = self.get_pixel_color((x, y))
+            if color_ == color:
+                locations.append((x, y))
+
+            if x == x2:
+                x = x1
+                y += step_y
+
+            x += step_x
+        return locations
+
+    def set_pixel_color(self, color, position=None):
+        position = position or self._cursor.get_position()
+        colorref = _make_COLORREF(color)
+        gdi.SetPixel(self._dc, position[0], position[1], colorref)
